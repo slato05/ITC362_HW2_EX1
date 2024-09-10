0a5be89a7d6d44c1f791a90dce00f816af27e8b0
slato05 <slato2001@gmail.com>
2024-09-10 14:33:07 -0500

    Final working version 3 (code clean up)

--- app/src/main/java/com/example/itc362_hw2_ex1/MainActivity.kt
+++ app/src/main/java/com/example/itc362_hw2_ex1/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.itc362_hw2_ex1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import com.example.itc362_hw2_ex1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
-    //lateinit' allows initializing a not-null property outside of a constructor
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -20,21 +15,14 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
+
     private var currentIndex = 0
     private lateinit var binding:ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-
-        //https://developer.android.com/develop/ui/views/notifications/snackbar/action
 
         binding.trueButton.setOnClickListener{
             checkAnswer(true)
@@ -48,9 +36,6 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
-
-        updateQuestion()
-
     }
 
     private fun updateQuestion(){
